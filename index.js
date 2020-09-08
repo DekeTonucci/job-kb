ea12a91b23d847fb0d3dd2c0f2a678e5229c71d7
Deke <deke@Dekes-MacBook-Pro.local>
2020-09-08 14:25:49 -0400

    updated console log link

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const { getLinks, getPageData } = require('./helpers');
 const keyword = 'web'; // Job title or category ect...
 const minSalary = '$30,000'; // Job title or category ect...
-const zipcode = '32830'; // 5 digit code
+const zipcode = '18974'; // 5 digit code
 const radius = '5'; // 5 - 10 - 15 - 25 - 50 - 100
 const experienceLevel = 'entry_level'; // entry_level - mid_level - senior_level
 
@@ -13,7 +13,7 @@ const indeedRemoteURL = `https://www.indeed.com/jobs?q=${keyword}+${minSalary}&l
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   const url = indeedRemoteURL;
-  console.log('Indeed Link: ', indeedURL);
+  console.log('Indeed Link: ', url);
   const allLinks = await getLinks(url, page);
 
   const scrapedData = [];
